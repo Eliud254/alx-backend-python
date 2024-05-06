a10f4fd52e80d3e7e1a81b08592d8b39c9859a52
Eliud254 <eliudobure@gmail.com>
2024-05-06 19:06:15 +0000

    Pycodestyle test done

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python3
-"""
-Includes a coroutine that halts execution for a specified period before yielding control back.
-"""
-
+""" Module documentation """
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """
-    This function creates a random decimal number between 0 and a specified maximum delay.
-
-    Parameters:
-    - max_delay: The upper limit for the delay time.
-
-    Returns:
-    - A randomly generated decimal number within the range of 0 to max_delay.
-    """
-    # Generate a random delay between 0 and max_delay
-    rand = random.uniform(0, max_delay)
-    # Asynchronously sleep for the generated delay
-    await asyncio.sleep(rand)
-    # Return the generated delay
-    return rand
-
+    """document function"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
